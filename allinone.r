04d710182f3c9cd8faeb2121e9c40884a8dacd59
Richard Francais <richard.francais@student.csulb.edu>
2019-08-24 15:14:13 -0700

    Completed Data Visualizations

--- allinone.r
+++ allinone.r
@@ -1,4 +1,4 @@
-
+setwd("D:/RLearning/All-in-one")
 install.packages("XML")
 install.packages("RCurl")
 install.packages("plyr")
@@ -10,10 +10,9 @@ library(ggplot2)
 url <- "https://www.basketball-reference.com/leagues/NBA_2019_per_game.html"
 # These produce the same results: url <- "https://stats.nba.com/leaders/"
 urldata <-  getURL(url)
-data <- readHTMLTable(urldata, stringsAsFactors = FALSE)
+data <- readHTMLTable(urldata, stringsAsFactors = FALSE, encoding = "UTF-8")
 dat <- structure(data, row.names = c(NA, -734), .Names = seq_along(data), class = "data.frame")
-#This one does not work
-dat3 <- as.data.frame(data, col.names = names(data), headers = TRUE)
+#This one does not work dat3 <- as.data.frame(data, col.names = names(data), headers = TRUE)
 #This one works - But all variables are characteristic
 stats18 <- ldply(data, data.frame)
 # Adjusting variable names
@@ -27,28 +26,55 @@ colnames(stats18)[18] <- "2Ppct"
 colnames(stats18)[19] <- "eFG"
 colnames(stats18)[22] <- "FTpct"
 #Removing irrelevant variables, NAs, and converting to numeric
-statsbind2$.id <- NULL
-statsbind2$Rk <- NULL
-statsbind2 <- as.data.frame(sapply(statsbind2[1:31], as.numeric))
-statsbind2[is.na(statsbind2)] <- 0
+stats18$.id <- NULL
+stats18$Rk <- NULL
+##Numeric needs to ignore the character variables
+stats18numeric <- as.data.frame(sapply(stats18[c(3,5:29)], as.numeric))
+stats18numeric[is.na(stats18numeric)] <- 0
 #Isolating all traded players that show up as double in the data set 
 TOTplayer <- subset(stats18, Tm == "TOT")
 #Cleaning traded duplicates and adding in traded players totals
-statsTraded <- subset(stats18, Player %in% c("Ryan Anderson", "Trevor Ariza", "Ron Baker", "Harrison Barnes", "Avery Bradley", "Corey Brewer", "Reggie Bullock", "Trey Burke", "Alec Burks", "Jimmy Butler", "Isiah Canaan", "Michael Carter-Williams", "Tyson Chandler", "Wilson Chandler", "Marquese Chriss", "Robert Covington", "Mitch Creek", "Sam Dekker", "Matthew Dellavedova", "Tyler Dorsey", "Henry Ellenson", "Wayne Ellington", "James Ennis", "Jawun Evans", "Kenneth Faried", "Tim Frazier", "Marc Gasol", "Pau Gasol", "JaMychal Green", "Tim Hardaway", "Tobias Harris", "Andrew Harrison", "George Hill", "Justin Holiday", "Rodney Hood", "Justin Jackson", "John Jenkins", "B.J. Johnson", "Stanley Johnson", "Tyler Johnson", "Wesley Johnson", "DeAndre Jordan","Enes Kanter", "Brandon Knight", "Kyle Korver", "Skal Labissiere", "Courtney Lee", "Jeremy Lin", "Timothe Luwawu-Cabarrot", "Shelvin Mack", "Thon Maker", "Boban Marjanovic", "Wesley Matthews", "Patrick McCaw", "C.J. Miles", "Nikola Mirotic", "Greg Monroe", "Eric Moreland", "Markieff Morris", "Mike Muscala", "Sviatoslav Mykhailiuk", "James Nunnally", "Kelly Oubre", "Jabari Parker", "Cameron Payne", "Otto Porter", "Bobby Portis", "Austin Rivers", "Dario Saric", "Mike Scott", "Wayme Selden", "Landry Shamet", "Iman Shumpert", "Jonathon Simmons", "Dennis Smith", "Jason Smith", "Ray Spalding", "Nik Stauskas", "Caleb Swanigan", "Garrett Temple", "Emanuel Terry", "Jonas Valanciunas", "Christian Wood", "Delon Wright", "Tyler Zeller", "Ivica Zubac"))
+statsTraded <- subset(stats18, Player %in% c("Ryan Anderson", "Trevor Ariza", "Ron Baker", "Harrison Barnes", "Avery Bradley", "Corey Brewer", "Reggie Bullock", "Trey Burke", "Alec Burks", "Jimmy Butler", "Isiah Canaan", "Michael Carter-Williams", "Tyson Chandler", "Wilson Chandler", "Marquese Chriss", "Robert Covington", "Mitch Creek", "Sam Dekker", "Matthew Dellavedova", "Tyler Dorsey", "Henry Ellenson", "Wayne Ellington", "James Ennis", "Jawun Evans", "Kenneth Faried", "Tim Frazier", "Marc Gasol", "Pau Gasol", "JaMychal Green", "Tim Hardaway", "Tobias Harris", "Andrew Harrison", "George Hill", "Justin Holiday", "Rodney Hood", "Justin Jackson", "John Jenkins", "B.J. Johnson", "Stanley Johnson", "Tyler Johnson", "Wesley Johnson", "DeAndre Jordan","Enes Kanter", "Brandon Knight", "Kyle Korver", "Skal Labissiere", "Courtney Lee", "Jeremy Lin", "Timothe Luwawu-Cabarrot", "Shelvin Mack", "Thon Maker", "Boban Marjanovic", "Wesley Matthews", "Patrick McCaw", "C.J. Miles", "Nikola Mirotic", "Greg Monroe", "Eric Moreland", "Markieff Morris", "Mike Muscala", "Sviatoslav Mykhailiuk", "James Nunnally", "Kelly Oubre", "Jabari Parker", "Cameron Payne", "Otto Porter", "Bobby Portis", "Austin Rivers", "Dario Saric", "Mike Scott", "Wayme Selden", "Landry Shamet", "Iman Shumpert", "Jonathon Simmons", "Dennis Smith", "Jason Smith", "Ray Spalding", "Nik Stauskas", "Caleb Swanigan", "Garrett Temple", "Emanuel Terry", "Jonas Valančiūnas", "Christian Wood", "Delon Wright", "Tyler Zeller", "Ivica Zubac"))
 "%ni%" <- Negate("%in%")
-statsnotTraded <- subset(stats18, Player %ni% c("Ryan Anderson", "Trevor Ariza", "Ron Baker", "Harrison Barnes", "Avery Bradley", "Corey Brewer", "Reggie Bullock", "Trey Burke", "Alec Burks", "Jimmy Butler", "Isiah Canaan", "Michael Carter-Williams", "Tyson Chandler", "Wilson Chandler", "Marquese Chriss", "Robert Covington", "Mitch Creek", "Sam Dekker", "Matthew Dellavedova", "Tyler Dorsey", "Henry Ellenson", "Wayne Ellington", "James Ennis", "Jawun Evans", "Kenneth Faried", "Tim Frazier", "Marc Gasol", "Pau Gasol", "JaMychal Green", "Tim Hardaway", "Tobias Harris", "Andrew Harrison", "George Hill", "Justin Holiday", "Rodney Hood", "Justin Jackson", "John Jenkins", "B.J. Johnson", "Stanley Johnson", "Tyler Johnson", "Wesley Johnson", "DeAndre Jordan","Enes Kanter", "Brandon Knight", "Kyle Korver", "Skal Labissiere", "Courtney Lee", "Jeremy Lin", "Timothe Luwawu-Cabarrot", "Shelvin Mack", "Thon Maker", "Boban Marjanovic", "Wesley Matthews", "Patrick McCaw", "C.J. Miles", "Nikola Mirotic", "Greg Monroe", "Eric Moreland", "Markieff Morris", "Mike Muscala", "Sviatoslav Mykhailiuk", "James Nunnally", "Kelly Oubre", "Jabari Parker", "Cameron Payne", "Otto Porter", "Bobby Portis", "Austin Rivers", "Dario Saric", "Mike Scott", "Wayme Selden", "Landry Shamet", "Iman Shumpert", "Jonathon Simmons", "Dennis Smith", "Jason Smith", "Ray Spalding", "Nik Stauskas", "Caleb Swanigan", "Garrett Temple", "Emanuel Terry", "Jonas Valanciunas", "Christian Wood", "Delon Wright", "Tyler Zeller", "Ivica Zubac"))
+statsnotTraded <- subset(stats18, Player %ni% c("Ryan Anderson", "Trevor Ariza", "Ron Baker", "Harrison Barnes", "Avery Bradley", "Corey Brewer", "Reggie Bullock", "Trey Burke", "Alec Burks", "Jimmy Butler", "Isiah Canaan", "Michael Carter-Williams", "Tyson Chandler", "Wilson Chandler", "Marquese Chriss", "Robert Covington", "Mitch Creek", "Sam Dekker", "Matthew Dellavedova", "Tyler Dorsey", "Henry Ellenson", "Wayne Ellington", "James Ennis", "Jawun Evans", "Kenneth Faried", "Tim Frazier", "Marc Gasol", "Pau Gasol", "JaMychal Green", "Tim Hardaway", "Tobias Harris", "Andrew Harrison", "George Hill", "Justin Holiday", "Rodney Hood", "Justin Jackson", "John Jenkins", "B.J. Johnson", "Stanley Johnson", "Tyler Johnson", "Wesley Johnson", "DeAndre Jordan","Enes Kanter", "Brandon Knight", "Kyle Korver", "Skal Labissiere", "Courtney Lee", "Jeremy Lin", "Timothe Luwawu-Cabarrot", "Shelvin Mack", "Thon Maker", "Boban Marjanovic", "Wesley Matthews", "Patrick McCaw", "C.J. Miles", "Nikola Mirotic", "Greg Monroe", "Eric Moreland", "Markieff Morris", "Mike Muscala", "Sviatoslav Mykhailiuk", "James Nunnally", "Kelly Oubre", "Jabari Parker", "Cameron Payne", "Otto Porter", "Bobby Portis", "Austin Rivers", "Dario Saric", "Mike Scott", "Wayme Selden", "Landry Shamet", "Iman Shumpert", "Jonathon Simmons", "Dennis Smith", "Jason Smith", "Ray Spalding", "Nik Stauskas", "Caleb Swanigan", "Garrett Temple", "Emanuel Terry", "Jonas Valančiūnas", "Christian Wood", "Delon Wright", "Tyler Zeller", "Ivica Zubac"))
 statsbind <- rbind(statsnotTraded, TOTplayer)
 #Eliminating the extraneous "player" headers from the HTML Table
 statsbind2 <- subset(statsbind, Player %ni% c("Player"))
+statsbindcharacters <- subset(statsbind, Player %ni% c("Player"))
 #Calculating Z Scores
+statsbind2 <- as.data.frame(sapply(statsbind2[c(3,5:29)], as.numeric))
+statsbind2[is.na(statsbind2)] <- 0
 statsbind2$PTSZ <- (statsbind2$PTS - mean(statsbind2$PTS))/sd(statsbind2$PTS)
-statsbindScale <- scale(statsbind2[,5:29])
-statscbindScale <- cbind(statsbind2[c(1,2,4)], statsbindScale)
+statsbindScale <- scale(statsbind2[,1:27])
+##Problem here is that they need identical rows so the orignal needs to be subset
+##
+statscbindScale <- cbind(statsbindcharacters[c(1,2,4)], statsbindScale)
 statscbindScale$NegTOV <- statscbindScale$TOV * -1
 statscbindScale$NegPF <- statscbindScale$PF * -1
 ##Creating the all-in-one value
-statscbindScale[,31] <- rowSums(statscbindScale[c(10,13,16,17,20,21,23,24,25,28,29,30)])
-statscbindScale$combined_Z <- statscbindScale[,31]/12
-##Data visualization
+statscbindScale[,33] <- rowSums(statscbindScale[c(8,11,14,17,18,23,24,25,26,29,31,32)])
+statscbindScale$combined_Z <- statscbindScale[,33]/12
+##Top 20/Bottom 20 Diverging Bars Visualization
+statscombined_Z <- subset(statscbindScale, combined_Z < -0.7099 | combined_Z > 1.06)
 statscbindScale$abovebelow_Z <- ifelse(statscbindScale$combined_Z < 0, "below", "above")
-ggplot(subset(statscbindScale), aes(x=Player, y=combined_Z, label="Combined Z Score")) + geom_bar(stat='identity', aes(fill=abovebelow_Z), width=.5) + scale_fill_manual(name="Mileage", labels=c("Above Average", "Below Average"), values=c("above"="#FF2E00", "below"="#0078FF")) +labs(title ="Normalized Production for the 2018-19 NBA Season") + coord_flip()
+statscombined_Z$abovebelow_Z <- ifelse(statscombined_Z$combined_Z < 0, "below", "above")
+ggplot(statscombined_Z, aes(x=reorder(Player,combined_Z), y=combined_Z, label="Combined Z Score")) + geom_bar(stat='identity', aes(fill=abovebelow_Z), width=.5) + scale_fill_manual(name="Mileage", labels=c("Above Average", "Below Average"), values=c("above"="#FF2E00", "below"="#0078FF")) +labs(title ="Normalized Production for the 2018-19 NBA Season") + coord_flip() + xlab("Player")
+##Diverging Bars players with over 20 games played
+statscombined_Zgames <- subset(statscbindScale, G > -1.095 & (combined_Z < -.5262 | combined_Z > 1.08))
+statscombined_Zgames$abovebelow_Z <- ifelse(statscombined_Zgames$combined_Z < 0, "below", "above")
+ggplot(statscombined_Zgames, aes(x=reorder(Player,combined_Z), y=combined_Z, label="Combined Z Score")) + geom_bar(stat='identity', aes(fill=abovebelow_Z), width=.5) + scale_fill_manual(name="Mileage", labels=c("Above Average", "Below Average"), values=c("above"="#FF2E00", "below"="#0078FF")) +labs(title ="Normalized Production for the 2018-19 NBA Season", subtitle = "Players with over 20 games played") + coord_flip() + xlab("Player")
+#Top 25 Players by Normalized Position 
+ggplot(subset(statscbindScale, combined_Z > 1.02), aes(x=reorder(Player,combined_Z), y=combined_Z, label="Combined Z Score")) + geom_bar(stat='identity', aes(fill=Pos), width=.5) + scale_fill_manual(name="Position", labels=c("Center", "Power Forward", "Point Guard", "Small Forward", "Shooting Guard"), values=c("PG"="#C49A00", "SG"="#00B6EB", "SF"="#FB61D7", "PF"="#53B400","C"="#F8766D")) +labs(title ="Top 25 Players by Normalized Production") + coord_flip() + xlab('Player') + ylab('Combined Z Score')
+#Pie Chart and Frequency table
+freqtable <- c(10/25,3/25,5/25,4/25,3/25)
+freqmatrix <- matrix(freqtable, ncol=5, byrow = TRUE)
+freqmatrix <- t(freqmatrix)
+freqframe[,2] <- c("Center", "Power Forward", "Point Guard", "Small Forward", "Shooting Guard")
+names(freqframe)[1] <- "Prob"
+names(freqframe)[2] <- "PlayerPosition"
+ggplot(freqframe, aes(x="", y= Prob, fill = factor(PlayerPosition))) + geom_bar(width = 1, stat = "identity") + theme(axis.line = element_blank(), plot.title = element_text(hjust = 0.5), axis.text.x = element_blank()) + labs(fill = "Position", x = NULL, y = NULL, title = "Proportion of Positions in the Top 25 Players") + coord_polar(theta = "y", start = 0) + geom_text(aes(label=Prob), position = position_stack(vjust = 0.5))
+##Position totals AKA visualizing why centers are overrepresented
+statscbindNoScale <- cbind(statsbind555[c(1,2,4)], statsbind2)
+positiontotals <- tapply(statscbindNoScale$FG, statscbindNoScale$Pos, FUN = sum)
+positionaggregates <- aggregate(. ~ statscbindNoScale$Pos, statscbindNoScale[c(10,13,16,17,20,21,23,24,25,28,29)], sum)
+positionaggregates5 <- subset(positionaggregates, FGpct > 1)
